66f0117286570bd534c26bd5753f77cdb2cb8943
Jeremias Ezequiel <jeremias150817@gmail.com>
2025-04-07 23:53:17 -0300

    feat: added validation to input field in the expense form

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-const price = document.getElementById('price');
-const quantity = document.getElementById('quantity');
-const total = document.getElementById('total');
+const total = document.getElementById("total");
+const formExpense = document.getElementById("formExpense");
 
-function updateTotal(){
-    const totalPrice = parseInt(price.value) * parseInt(quantity.value); 
-    total.textContent = totalPrice; 
+const price = document.getElementById("price");
+const quantity = document.getElementById("quantity");
+const description = document.getElementById("description");
+const category = document.getElementById("category");
+
+const inputsExpense = [price, quantity, description, category];
+
+price.addEventListener("input", updateTotal);
+quantity.addEventListener("input", updateTotal);
+
+inputsExpense.forEach((input) =>
+  input.addEventListener("input", (event) => {
+    const { id, value } = event.target;
+
+    if (value === "") showError(id, event.target);
+  })
+);
+
+formExpense.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+function updateTotal() {
+  const totalPrice = parseInt(price.value) * parseInt(quantity.value);
+  total.textContent = totalPrice;
 }
 
+function showError(id, parent) {
+  const existingError = parent.parentElement.querySelector(".bg-danger");
 
-price.addEventListener('input',updateTotal);
-quantity.addEventListener('input',updateTotal);
+  if (!existingError) {
+    const error = document.createElement("P");
+    error.textContent = `The ${id} field is empty`;
+    error.classList.add(
+      "text-light",
+      "bg-danger",
+      "p-2",
+      "m-2",
+      "rounded",
+      "text-center",
+      "fw-bold"
+    );
+    parent.parentElement.appendChild(error);
 
+    setTimeout(() => {
+      error.remove();
+    }, 2000);
+  }
+}
